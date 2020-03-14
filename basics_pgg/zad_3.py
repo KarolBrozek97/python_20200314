6bee1836ec8c3156d417c97c889915b3508f8f3f
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2020-03-14 14:59:45 +0100

    Basic zad 2 i 3

---
+++ basics_pgg/zad_3.py
@@ -0,0 +1,11 @@
+# Napisz program wypisujący na konsolę Twoje imię i wzrost.
+# Do przechowywania informacji o Twoim imieniu i wzroście użyj
+# zmiennych.
+
+imie = "Piotr"
+wzrost = 220
+
+print("Imie:", imie) # do printa moge podawac wiele argumentow i python je wszystkie wyswietli i oddzieli spacja
+print("Imie: " + imie)
+print("Wzrost: " + str(wzrost)) # mozemy konkatenowac tylko str z str, nie z liczba, dlatego trzeba liczbe przerobic na string
+print("Wzrost:", wzrost)
